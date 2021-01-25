06fbffdde66ce632b3991341fe1cd0697e378b22
Toha <tohenk@yahoo.com>
2021-01-26 01:28:34 +0700

    Initial import.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# webapps-win-installer
-WebApps Windows Installer (Apache HTTP + PHP)
+# WebApps Windows Installer
+
+## Introduction
+
+WebApps Windows Installer provides script to help install Apache HTTPD and PHP
+into Windows operating system. It usages mainly to allow easy install or update
+Apache HTTPD and PHP by dropping downloaded archive into `Installer`
+folder.
+
+WebApps Windows Installer supports binaries downloaded from:
+* [Apache Lounge](https://www.apachelounge.com/download/)
+* [Windows PHP](https://windows.php.net/download/)
+* [PECL extension](https://windows.php.net/downloads/pecl/releases/)
+
+It's required, prior to use the script to install
+[Microsoft Visual C++ Redistributable](https://support.microsoft.com/en-us/topic/the-latest-supported-visual-c-downloads-2647da03-1eea-4433-9aff-95f26a218cc0).
+
+## Usage
+
+To install or update Apache HTTPD and PHP, open an *Administrator Command Prompt*
+and issue:
+
+```
+cd x:\path\to\installer
+install.cmd VC16
+```
---
+++ switch_app.cmd
@@ -0,0 +1,121 @@
+@echo off
+
+:: switch_app.cmd version 1.0
+:: Copyright (c) 2021 Toha <tohenk@yahoo.com>
+
+title Web Application Switcher
+echo Web Application Switcher v1.0
+echo (c) 2021 Toha ^<tohenk@yahoo.com^>
+echo ================================
+echo.
+
+setlocal
+
+:: change to current directory; vista run as administrator always start from SYSDIR
+%~d0
+cd %~dp0
+set CD=%~dp0
+set SERVICE=Apache2.4
+
+if [%1]==[] goto nocfgpath
+set CONFIG_PATH=%1
+goto :setcd
+
+:nocfgpath
+set CONFIG_PATH=%cd%Conf\vhosts
+
+:setcd
+set CURDIR=%~dp0
+echo Path to configuration:
+echo %CONFIG_PATH%
+echo.
+
+call :ch_drive "%CONFIG_PATH%"
+call :list_app
+call :choose_app %DEFAULT_APP%
+
+if not [%DEFAULT_APP%]==[%APP%] (
+    echo.
+    echo Please wait, applying changes...
+    call :update_app %APP%
+    call :restart_service %SERVICE%
+)
+
+call :ch_drive "%CURDIR%"
+goto :end
+
+:ch_drive
+    %~d1
+    cd %1
+    goto :eof
+
+:list_app
+    echo Available applications:
+    for /r %%i in (*.*) do (
+        if [%%~xi]==[.conf] (
+            echo - %%~ni [*]
+            set DEFAULT_APP=%%~ni
+        ) else (
+            echo - %%~ni
+        )
+    )
+    echo.
+
+    goto :eof
+
+:choose_app
+    set APP=%1
+    :choose
+    set /p app="Choose application [%APP%]: "
+    for /r %%i in (*.*) do (
+        if [%%~ni]==[%APP%] (
+            goto :eof
+        )
+    )
+    goto :choose
+
+:update_app
+    for /r %%i in (*.*) do (
+        if [%%~ni]==[%1] (
+            if [%%~xi]==[.conf-] call :rename_conf enable "%%i"
+        ) else (
+            if [%%~xi]==[.conf] call :rename_conf disable "%%i"
+        )
+    )
+    goto :eof
+
+:update_conf
+    set UPDATE_TYPE=
+    set UPDATE_MSG=
+    if [%~x1]==[.conf] (
+        set UPDATE_TYPE=disable
+        set UPDATE_MSG=Disable %~n1
+    )
+    if [%~x1]==[.conf-] (
+        set UPDATE_TYPE=enable
+        set UPDATE_MSG=Enable %~n1
+    )
+    :loop
+    set /p ANSWER="%UPDATE_MSG% [y/n]: "
+    if [%ANSWER%]==[y] goto :okay
+    if [%ANSWER%]==[n] goto :okay
+    goto :loop
+    :okay
+    if [%ANSWER%]==[y] (
+        call :rename_conf %UPDATE_TYPE% %1
+    )
+    goto :eof
+
+:rename_conf
+    if [%1]==[disable] set NEW_NAME=%~n2.conf-
+    if [%1]==[enable] set NEW_NAME=%~n2.conf
+    ren %2 %NEW_NAME%
+    goto :eof
+
+:restart_service
+    net stop %1
+    net start %1
+    goto :eof
+
+:end
+endlocal
